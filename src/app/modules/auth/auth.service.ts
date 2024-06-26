7aae8d2bdc3dec2e0c7ab0e4f7a96559cc3b28da
sm0bin <shehjad0mobin@gmail.com>
2024-06-26 23:37:20 +0600

    auth

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,30 +3,31 @@ import { ILoginUser } from "./auth.interface";
 import { User } from "../users/user.model";
 
 const loginUser = async (payload: ILoginUser) => {
-    const user = await User.findOne({ payload.email });
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User not found");
-    }
+  const { email, password } = payload;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
 
-    if (user.password !== payload.password) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
-    }
-    // const isPasswordMatch = await user.comparePassword(payload.password);
+  if (user.password !== password) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
+  }
+  // const isPasswordMatch = await user.comparePassword(payload.password);
 
-    // if (!isPasswordMatch) {
-    //     throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
-    // }
+  // if (!isPasswordMatch) {
+  //     throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
+  // }
 
-    return user;
+  return user;
 };
 
 const signupUser = async (payload: any) => {
-    const user = await User.create(payload);
+  const user = await User.create(payload);
 
-    return user;
+  return user;
 };
 
 export const AuthServices = {
-    loginUser,
-    signupUser,
+  loginUser,
+  signupUser,
 };
